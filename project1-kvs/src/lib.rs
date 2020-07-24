5a69681da1293d5d83de58ba6bddbb4d669f7853
Tim Deeb-Swihart <tim@deebswih.art>
2020-07-24 11:56:20 -0400

    Added documentation and a single doc test

--- project1-kvs/src/lib.rs
+++ project1-kvs/src/lib.rs
@@ -1,5 +1,17 @@
 use std::collections::HashMap;
 
+/// A string to string key-value store
+///
+/// Key-value pairs are stored in memory and not persisted to disk.
+///
+/// Example usage:
+/// ```rust
+/// # use kvs::KvStore;
+/// let mut store = KvStore::new();
+/// store.set("my key".to_owned(), "my value".to_owned());
+/// let val = store.get("my key".to_owned());
+/// assert_eq!(val, Some("my value".to_owned()));
+///```
 pub struct KvStore {
     store: HashMap<String, String>,
 }
@@ -11,6 +23,9 @@ impl KvStore {
         }
     }
 
+    /// Retrieve the value stored at the specified key
+    ///
+    /// Returns `None` if the key does not exist.
     pub fn get(&self, key: String) -> Option<String> {
         match self.store.get(&key) {
             Some(v) => Some(v.to_owned()),
@@ -18,10 +33,16 @@ impl KvStore {
         }
     }
 
+    /// Set the value for the specified key.
+    ///
+    /// If a value is already stored at this key it is unceremoniously overwritten.
     pub fn set(&mut self, key: String, value: String) {
         self.store.insert(key, value);
     }
 
+    /// Remove the value stored under the specified key.
+    ///
+    /// If nothing is stored at that key nothing happens.
     pub fn remove(&mut self, key: String) {
         self.store.remove(&key);
     }
