9e338f2fb3d78a04f059a48f4a3de1f4ed2cec2c
Tim Deeb-Swihart <tim@deebswih.art>
2020-08-09 20:41:58 -0400

    Compiling

--- kvs/src/lib.rs
+++ kvs/src/lib.rs
@@ -1,4 +1,17 @@
+use snafu::{ResultExt, Snafu};
 use std::collections::HashMap;
+use std::io;
+use std::path::PathBuf;
+
+#[derive(Debug, Snafu)]
+pub enum Error {
+    #[snafu(display("io error writing to {}: {}", path.display(), source))]
+    Write { source: io::Error, path: PathBuf },
+    #[snafu(display("io error reading from {}: {}", path.display(), source))]
+    Read { source: io::Error, path: PathBuf },
+}
+
+pub type Result<T, E = Error> = std::result::Result<T, E>;
 
 /// A string to string key-value store
 ///
@@ -7,9 +20,9 @@ use std::collections::HashMap;
 /// Example usage:
 /// ```rust
 /// # use kvs::KvStore;
-/// let mut store = KvStore::new();
-/// store.set("my key".to_owned(), "my value".to_owned());
-/// let val = store.get("my key".to_owned());
+/// let mut store = KvStore::open("/tmp/log.kv")?;
+/// store.set("my key".to_owned(), "my value".to_owned())?;
+/// let val = store.get("my key".to_owned())?;
 /// assert_eq!(val, Some("my value".to_owned()));
 ///```
 pub struct KvStore {
@@ -17,27 +30,26 @@ pub struct KvStore {
 }
 
 impl KvStore {
-    pub fn new() -> KvStore {
-        KvStore {
-            store: HashMap::new(),
-        }
+    pub fn open(_path: impl Into<PathBuf>) -> Result<KvStore> {
+        unimplemented!();
     }
 
     /// Retrieve the value stored at the specified key
     ///
     /// Returns `None` if the key does not exist.
-    pub fn get(&self, key: String) -> Option<String> {
-        match self.store.get(&key) {
+    pub fn get(&self, key: String) -> Result<Option<String>> {
+        Ok(match self.store.get(&key) {
             Some(v) => Some(v.to_owned()),
             None => None,
-        }
+        })
     }
 
     /// Set the value for the specified key.
     ///
     /// If a value is already stored at this key it is unceremoniously overwritten.
-    pub fn set(&mut self, key: String, value: String) {
+    pub fn set(&mut self, key: String, value: String) -> Result<()> {
         self.store.insert(key, value);
+        Ok(())
     }
 
     /// Remove the value stored under the specified key.
@@ -47,13 +59,14 @@ impl KvStore {
     /// # Example
     /// ```rust
     /// # use kvs::KvStore;
-    /// let mut store = KvStore::new();
-    /// store.set("my key".to_owned(), "my value".to_owned());
-    /// store.remove("my key".to_owned());
-    /// let val = store.get("my key".to_owned());
+    /// let mut store = KvStore::open("/tmp/log.kv")?;
+    /// store.set("my key".to_owned(), "my value".to_owned())?;
+    /// store.remove("my key".to_owned())?;
+    /// let val = store.get("my key".to_owned())?;
     /// assert_eq!(val, None);
     /// ```
-    pub fn remove(&mut self, key: String) {
+    pub fn remove(&mut self, key: String) -> Result<()> {
         self.store.remove(&key);
+        Ok(())
     }
 }
